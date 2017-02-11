712709d935af9fdd9df0f11dfa73cc606db41fe5
Kevin Chaplin <kevinchaplin58@gmail.com>
2017-02-11 00:46:13 -0500

    what

--- runserver.py
+++ runserver.py
@@ -1,9 +1,9 @@
 """
-This script runs the FlaskWebProject application using a development server.
+This script runs the FlaskWebProject1 application using a development server.
 """
 
 from os import environ
-from FlaskWebProject import app
+from FlaskWebProject1 import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
